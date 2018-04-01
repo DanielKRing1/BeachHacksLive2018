5069d5335694ccdf00f1669991b50861006c4c43
DanielKRing1 <DanielKRing1@gmail.com>
2018-04-01 16:12:09 -0700

    Removed duplicate 'ids' prevented mentor request from submitting

--- beachlive/announcements/announcements_controller.js
+++ beachlive/announcements/announcements_controller.js
@@ -1,9 +1,22 @@
-liveApp.controller('announcements_controller', ["$scope","$timeout","$firebaseArray",function($scope,$timeout,$firebaseArray){
+liveApp.controller('announcements_controller', ["$scope","$timeout","$filter","$firebaseArray",function($scope,$timeout,$filter,$firebaseArray){
 		console.log("hi admin annoucement controller");
+
 
 		var rootRef = firebase.database().ref();
     var ref = rootRef.child("announcements");
-		$scope.announcementList = $firebaseArray(ref);
+		$scope.announcementList;
+
+		var annCount;
+
+		window.onload = function(){
+			$scope.announcementList = $firebaseArray(ref);
+
+			$scope.announcementList.$loaded(
+				function(data) {
+					annCount = data.length;
+					setInterval(checkForNewAnn, 1000);
+				});
+		};
 
     $scope.addAnnouncement = function() {
 			if($scope.newAnnouncement !== ''){
@@ -12,7 +25,36 @@ liveApp.controller('announcements_controller', ["$scope","$timeout","$firebaseAr
 						 a.description = $scope.newAnnouncement;
 						 ref.push(a);
 						 $scope.newAnnouncement = '';
+
 			}
-  };
+		};
+
+
+		// Notification display
+		function checkForNewAnn(){
+			if(annCount < $scope.announcementList.length){
+				annCount = $scope.announcementList.length;
+				displayNotification($scope.announcementList[0]);
+			}
+		};
+
+
+		function displayNotification(a){
+
+			if(Notification.permission === 'granted'){
+				navigator.serviceWorker.getRegistration().then(function(reg){
+
+					var body = $filter('date')(a.time, 'h:mma')+" | "+a.description;
+
+					reg.showNotification('BeachHacks 2018', {
+							icon: 'https://i.imgur.com/yHASg5C.png',
+							body: body,
+							vibrate: [100, 50, 100],
+					actions: [
+						{action: 'close', title: 'Close notification'}
+					]});
+				});
+			}
+		};
 
 }]);
