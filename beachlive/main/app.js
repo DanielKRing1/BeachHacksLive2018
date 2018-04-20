e4013fab2cef0f8bad69f681cc4aeb144c341a9a
DanielKRing1 <DanielKRing1@gmail.com>
2018-04-20 14:29:23 -0700

    Created 'notif-ctrl' to send notif upon addition to firebase announcements

--- beachlive/main/app.js
+++ beachlive/main/app.js
@@ -62,31 +62,69 @@ liveApp.config(function($stateProvider, $urlRouterProvider) {
       			templateUrl: 'mentors/admin_mentors.html',
       			controller: 'mentors_controller'
         })
-
-
-
-	  // Check if NOTIFICATION API supported
-	  if ("Notification" in window) {
-	    // Get PERMISSION to display Notifications
-	    Notification.requestPermission().then(function(status){
-	      //console.log('Notification permission status: ', status);
-	    });
-	  }else{
-	    console.warn('This browser does not support Notifiation API')
-	  }
-
-	  // Check if PUSH API supported
-	  if ('serviceWorker' in navigator) {
-	    window.addEventListener('load', function() {
-	      // REGISTER Service Worker
-	      navigator.serviceWorker.register('sw.js').then(function(reg) {
-	        console.log('ServiceWorker registration successful: ', reg);
-	      }, function(err) {
-	        console.log('ServiceWorker registration failed: ', err);
-	      });
-	    });
-	  }else{
-	    console.warn('This browser does not have a registered Service Worker');
-	  }
-
 });
+
+liveApp.controller('notif-ctrl', ["$scope","$filter","$firebaseArray", function($scope,$filter,$firebaseArray){
+  // Check if NOTIFICATION API supported
+  if ("Notification" in window) {
+    // Get PERMISSION to display Notifications
+    Notification.requestPermission().then(function(status){
+      //console.log('Notification permission status: ', status);
+    });
+  }else{
+    console.warn('This browser does not support Notifiation API')
+  }
+
+  // Check if PUSH API supported
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      // REGISTER Service Worker
+      navigator.serviceWorker.register('sw.js').then(function(reg) {
+        // console.log('ServiceWorker registration successful: ', reg);
+      }, function(err) {
+        // console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }else{
+    console.warn('This browser does not have a registered Service Worker');
+  }
+  var rootRef = firebase.database().ref();
+  var ref = rootRef.child("announcements");
+  $scope.announcementList = $firebaseArray(ref);
+  $scope.announcementList.$loaded(
+    function(data) {
+      annCount = data.length;
+      setInterval(checkForNewAnn, 1000);
+    });
+
+    var annCount;
+
+    // Notification display
+    function checkForNewAnn(){
+      if(annCount < $scope.announcementList.length){
+        annCount = $scope.announcementList.length;
+        newestAnn = $scope.announcementList.length-1;
+
+        displayNotification($scope.announcementList[newestAnn]);
+      }
+    };
+
+    function displayNotification(a){
+
+      if(Notification.permission === 'granted'){
+        navigator.serviceWorker.getRegistration().then(function(reg){
+
+          var body = $filter('date')(a.time, 'h:mma')+" | "+a.description;
+
+          reg.showNotification('BeachHacks 2018', {
+            icon: 'https://i.imgur.com/yHASg5C.png',
+            body: body,
+            vibrate: [100, 50, 100],
+            actions: [
+              {action: 'close', title: 'Close notification'}
+            ]
+          });
+        });
+      }
+    };
+}]);
