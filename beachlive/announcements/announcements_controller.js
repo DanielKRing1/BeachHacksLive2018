043750008dd612c73f958d776eb283f3385cb4bf
DanielKRing1 <DanielKRing1@gmail.com>
2018-04-20 14:21:28 -0700

    Removed Notif logic (permissions, sending notif, and service worker registration)

--- beachlive/announcements/announcements_controller.js
+++ beachlive/announcements/announcements_controller.js
@@ -5,14 +5,6 @@ liveApp.controller('announcements_controller', ["$scope","$timeout","$filter","$
 		var rootRef = firebase.database().ref();
     var ref = rootRef.child("announcements");
 		$scope.announcementList = $firebaseArray(ref);
-		$scope.announcementList.$loaded(
-			function(data) {
-				annCount = data.length;
-				setInterval(checkForNewAnn, 1000);
-			});
-
-		var annCount;
-
 
     $scope.addAnnouncement = function() {
 			if($scope.newAnnouncement !== ''){
@@ -21,34 +13,6 @@ liveApp.controller('announcements_controller', ["$scope","$timeout","$filter","$
 						 a.description = $scope.newAnnouncement;
 						 ref.push(a);
 						 $scope.newAnnouncement = '';
-			}
-		};
-
-
-		// Notification display
-		function checkForNewAnn(){
-			if(annCount < $scope.announcementList.length){
-				annCount = $scope.announcementList.length;
-				newestAnn = $scope.announcementList.length-1;
-				displayNotification($scope.announcementList[newestAnn]);
-			}
-		};
-
-		function displayNotification(a){
-
-			if(Notification.permission === 'granted'){
-				navigator.serviceWorker.getRegistration().then(function(reg){
-
-					var body = $filter('date')(a.time, 'h:mma')+" | "+a.description;
-
-					reg.showNotification('BeachHacks 2018', {
-							icon: 'https://i.imgur.com/yHASg5C.png',
-							body: body,
-							vibrate: [100, 50, 100],
-					actions: [
-						{action: 'close', title: 'Close notification'}
-					]});
-				});
 			}
 		};
 
